91938cf56e7bc8a44e93721fddc9aa88c50b194c
Saeed <sdkysfzai@gmail.com>
2023-04-16 21:13:44 +0500

    update

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Notes App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
